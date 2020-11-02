bfed2dedaec9d830ddde37e5b2800bbba92a1497
jinhan38 <jinhan38@naver.com>
2020-11-02 22:39:24 +0900

    Initial commit

---
+++ app/src/main/java/com/databinding/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.databinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.databinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        fetchUserData()
+    }
+
+    private fun fetchUserData() {
+        var userProfile = UserProfile()
+        userProfile.name = "김미미"
+        userProfile.phone = "010-3333-8888"
+        userProfile.address = "인천광역시 계양구"
+
+        binding.userProfile = userProfile
+
+//        updateUser(userProfile)
+    }
+
+    private fun updateUser(userProfile: UserProfile) {
+        userProfile.apply {
+            binding.name.text = this.name
+            binding.phone.text = this.phone
+            binding.address.text = this.address
+        }
+
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+
+}
---
+++ app/src/main/java/com/databinding/UserProfile.kt
@@ -0,0 +1,7 @@
+package com.databinding
+
+data class UserProfile(
+    var name: String = "",
+    var phone: String = "",
+    var address: String = ""
+)
